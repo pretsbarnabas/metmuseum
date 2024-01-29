cb05b204d8be506d6a93b332d46e967b13e8d2b5
[Máté Varga] <[varga.mate.gellert@students.jedlik.eu]>
2024-01-29 11:27:44 +0100

    Classes

---
+++ Model/Constituent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace museum_api.Model
+{
+    public class Constituent
+    {
+        public int constituentID { get; set; }
+        public string role { get; set; }
+        public string name { get; set; }
+        public string constituentULAN_URL { get; set; }
+        public string constituentWikidata_URL { get; set; }
+        public string gender { get; set; }
+    }
+}
---
+++ Model/ElementMeasurements.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace museum_api.Model
+{
+    public class ElementMeasurements
+    {
+        public double Height { get; set; }
+        public double Width { get; set; }
+        public int? Depth { get; set; }
+    }
+}
